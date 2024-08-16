7b40399e67658f428d17c91f501a887b8405ecaa
Sameer-Programmer <115461857+Sameer-Programmer@users.noreply.github.com>
2024-08-16 17:57:31 +0000

    commit - @Aug-16-2024

--- Java_Basics/Package1/Test1.java
+++ Java_Basics/Package1/Test1.java
@@ -1,5 +1,19 @@
 public class Test1 {
+   
+
 public static void main(String[] args) {
     System.out.println("Sameer");
+    System.out.println("Imran");
 }
 }
+ // Test1 is the Class Name 
+    // simple Hello Word Programme
+    
+    // Public = Access Modifier
+    // static is the Key Word 
+    // void = Return type means it does not Returns any value
+    // main  = Method Name , It is the Entry point of Java Programm 
+    // String [] args = Array - command Line arguements 
+    //System is the Class Present in Java.lang package
+    // out is a static variable Present in System Class of type Print Stream 
+    // print ln is method Present in Print Stream Class 
---
+++ Java_Basics/Package1/Test2.java
@@ -0,0 +1,10 @@
+public class Test2 {
+    static String s = "sameer_bhai";
+public static void main (String [] args ) {
+    int Length = Test2.s.length();
+    System.out.println(Length+"   Length ");
+    }
+}
+
+
+
